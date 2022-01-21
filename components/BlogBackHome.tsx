334b69972b8530453aea668c2f3af7397c52d03c
Harry Yep <github@hode.uk>
2022-01-21 10:41:42 -0600

    fixed with Link

--- components/BlogBackHome.tsx
+++ components/BlogBackHome.tsx
@@ -1,13 +1,15 @@
+import Link from 'next/link';
+
 import { BiArrowBack } from 'react-icons/bi';
 
 const BlogBackHome = () => {
     return (
         <div id='notion-blog-back-home' className='container mx-auto max-w-screen-lg dark:text-[#adbac7]'>
-            <div className='flex flex-row items-center space-x-2'>
-                <BiArrowBack />
-                <a href='/' className='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600'>
-                    Back Home
-                </a>
+            <div className='flex'>
+                <span className='flex flex-row items-center space-x-2 transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600'>
+                    <BiArrowBack />
+                    <Link href='/'>Back Home</Link>{' '}
+                </span>
             </div>
         </div>
     );
