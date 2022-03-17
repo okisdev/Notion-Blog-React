10d7ae0b4dc6149ec6702dbc16cd2a52d83005d3
Harry Yep <github@hode.uk>
2022-03-17 17:40:26 +0000

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 module.exports = {
     i18n: {
         defaultLocale: 'en-GB',
         locales: ['en-GB', 'zh-CN'],
     },
+    localePath: path.resolve('./public/locales'),
 };
