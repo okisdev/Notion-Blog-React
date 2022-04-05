5e8fa41a723d769ab2f47042d1c5fe05271a8ab2
Harry Yep <github@hode.uk>
2022-04-05 11:13:05 +0100

    upgrade to react-notion-x@6.6.2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,8 @@ import '../styles/notion.css';
 // used for code syntax highlighting (optional)
 import 'prismjs/themes/prism-tomorrow.css';
 
-// used for collection views (optional)
-import 'rc-dropdown/assets/index.css';
-
 // used for rendering equations (optional)
-import 'katex/dist/katex.min.css';
+import 'react-notion-x/build/third-party/equation.css';
 
 import { appWithTranslation } from 'next-i18next';
 
